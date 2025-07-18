8261923873528817228ceeb917b725fe19b1fdf2
Adrian Salazar Collazos <adrian.1009@live.com>
2025-07-18 11:51:07 -0400

    Adrian: Fec: 0.25

--- Hurdles/RunExample.m
+++ Hurdles/RunExample.m
@@ -38,7 +38,7 @@ switch LidarType
         LDP.AngleToCenterline   = 19.176;       % [deg]     Angle around centerline
         LDP.IndexGate           = 10;            % [-]       IndexGate
         LDP.FlagLPF             = 1;            % [0/1]     Enable low-pass filter (flag)
-        LDP.omega_cutoff        = 0.15;         % [rad/s]   Corner frequency (-3dB) of the low-pass filter
+        LDP.omega_cutoff        = 0.25;         % [rad/s]   Corner frequency (-3dB) of the low-pass filter
         LDP.T_buffer            = 0.2;          % [s]       Buffer time for filtered REWS signal
     case 'CircularCW'
         % configuration from LDP_v1_CircularCW.IN and FFP_v1_CircularCW.IN
